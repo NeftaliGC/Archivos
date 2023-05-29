7c89d87e59a3f585bcc0a879749250a9a9cbff23
NeftaliGC <aliguiacruz@outlook.com>
2023-05-29 12:53:34 -0600

    cambio random para enseñarle a sofi

--- src/main/java/com/nintech/veterinaria/Veterinaria.java
+++ src/main/java/com/nintech/veterinaria/Veterinaria.java
@@ -3,6 +3,6 @@ package com.nintech.veterinaria;
 
 public class Veterinaria {
     public static void main(String[] args) {
-        System.out.println("Este es un nuevo mensaje");
+        System.out.println("Essdfasdfasdf");
     }
 }
